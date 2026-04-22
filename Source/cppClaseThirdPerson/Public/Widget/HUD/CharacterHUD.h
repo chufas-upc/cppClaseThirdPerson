0e4b2509203f3151040ca906a74ec1706f95768b
TheChufas <facundoagustinluduena@upc.edu.ar>
2026-04-21 21:21:10 -0300

    Clase 20/04 Line Trace

---
+++ Source/cppClaseThirdPerson/Public/Widget/HUD/CharacterHUD.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterHUD.generated.h"
+
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class CPPCLASETHIRDPERSON_API UCharacterHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TextInfo;
+	
+public:
+	void HideTextInfo();
+	void ShowItem(const char* Str);
+};
